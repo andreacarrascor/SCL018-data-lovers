b105fb0cecf87bbef0f8298b853508dd28937e93
andreacarrascor <ancarami19@gmail.com>
2021-09-29 17:01:53 -0300

    orden sintaxis código

--- src/data.js
+++ src/data.js
@@ -1,14 +1,14 @@
 const orderData = {
-  orderDefault: function (printFilm) {
-    let orderedResults = printFilm.sort(function (a, b) {
-    if (a.title > b.title) {
-      return 1; // retornar valores a la derecha (coordenadas)
-    } else {
-      return -1; // retornar valores a la izquierda (coordenadas)
-    }
-  });
-  return orderedResults;
-},
+    orderDefault: function (printFilm) {
+        let orderedResults = printFilm.sort(function (a, b) {
+            if (a.title > b.title) {
+                return 1; // retornar valores a la derecha (coordenadas)
+            } else {
+                return -1; // retornar valores a la izquierda (coordenadas)
+            }
+        });
+        return orderedResults;
+    },
 };
 
 export default orderData;
